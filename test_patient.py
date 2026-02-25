919ace9145f9c776de58b8fb04928a1551d75dfc
KWill44 <kwillhite@cub.uca.edu>
2026-02-25 16:04:14 -0600

    Created new functions to cover requirments 3,4, and 5 (Will uncomment them once the implementations that correspond with them are created in main.py)

--- test_patient.py
+++ test_patient.py
@@ -30,17 +30,87 @@ class TestPatientRegistry(unittest.TestCase):
 
 
     def test_register_invalid_name(self):
-        # REQ-01: Empty name raises ValueError
+        # REQ-01: Empty name raises ValueError - Willhite
         with self.assertRaises(ValueError):
             self.registry.register_patient("")
 
 
+
     def test_invalid_patient_id_format(self):
-        # REQ-02: Invalid patient ID format raises ValueError
+        # REQ-02: Invalid patient ID format raises ValueError - Willhite
         with self.assertRaises(ValueError):
             self.registry.get_patient("ABC")
+
+    """
+
+    def test_get_patient_by_id(self):
+    
+    # REQ-03: The system shall allow retrieving a patient record using their Patient ID. - Willhite
+    
+        test_name = "Charlie"
+        pid = self.registry.register_patient(test_name)
+        record = self.registry.get_patient(pid)
+
+        self.assertEqual(record["name"], test_name)
+        self.assertEqual(record["patient_id"], pid)
+    """
+
+
+
+
+    """
+
+    def test_update_patient_name(self):
+    
+         # REQ-04: Update a patient's name using Patient ID - Willhite
+    
+        pid = self.registry.register_patient("Alice")
+        updated_record = self.registry.update_patient_name(pid, "Alicia")
+
+        self.assertEqual(updated_record["name"], "Alicia")
+        self.assertEqual(pid, "P-101")
+
+    """
+    
+    """
+    def test_update_non_existent_patient(self):
+    
+        #REQ-04: Updating a non-existent patient raises KeyError - Willhite
+    
+        with self.assertRaises(KeyError):
+            self.registry.update_patient_name("P-9999", "Nobody")
+
+    """
+
+    """
+    def test_delete_patient(self):
+    
+        # REQ-05: Delete patient using Patient ID - Willhite
+    
+        pid = self.registry.register_patient("Bob")
+        result = self.registry.delete_patient(pid)
+
+        self.assertTrue(result)
+
+        # Confirm patient is deleted
+        with self.assertRaises(KeyError):
+            self.registry.get_patient(pid)
+
+    """
+
+    """
+
+    def test_delete_non_existent_patient(self):
+    
+        # REQ-05: Deleting non-existent patient raises KeyError - Willhite
+    
+        with self.assertRaises(KeyError):
+            self.registry.delete_patient("P-9999")
              
 
-            
+     """     
+
+
+
 if __name__ == '__main__':
     unittest.main()
